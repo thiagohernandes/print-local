fd349b61da3d4ffd7d273cb43fe198b0c19c5d4a
Thiago Hernandes <hernandes_sp@yahoo.com.br>
2020-08-06 08:45:56 -0300

    Configuração de POM

---
+++ api-print-local/src/test/java/com/br/print/ApiPrintLocalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.print;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPrintLocalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
