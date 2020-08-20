ca1aeb3ab2c27af6d06c3b42c9154ad2da488c4a
Thiago Hernandes <hernandes_sp@yahoo.com.br>
2020-08-20 17:07:47 -0300

    Ajustes de branch challege

--- api-print-local/src/main/java/com/br/print/configuration/security/TokenAuthenticationService.java
+++ api-print-local/src/main/java/com/br/print/configuration/security/TokenAuthenticationService.java
@@ -15,7 +15,7 @@ import io.jsonwebtoken.SignatureAlgorithm;
 public class TokenAuthenticationService {
 
     static final long EXPIRATION_TIME = 860_000_000;
-    static final String SECRET = "DesafioEvoluumSecret";
+    static final String SECRET = "DesafioSimplusSecret";
     static final String TOKEN_PREFIX = "Bearer";
     static final String HEADER_STRING = "Authorization";
 
